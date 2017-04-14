d705b945badb6d5618266f6cc5bca73b1e096062
Jeff Keen <jeff@keen.me>
2017-04-14 12:52:09 -0500

    Change action queue to wire up the event on the object instead of having that be a separate call

--- addon/services/action-queue.js
+++ addon/services/action-queue.js
@@ -1,24 +1,61 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
 import PromiseRace from 'ember-hifi/utils/promise-race';
-const { get, getWithDefault, set } = Ember;
+const { get, getWithDefault, set, assert } = Ember;
 
-export default Ember.Service.extend({
+export default Ember.Service.extend(Ember.Evented, {
   queues: {},
 
-  addAction(queueName, info, callback) {
-    let queue = getWithDefault(get(this, 'queues'), queueName, []);
+  addAction(thing, eventName, info, callback) {
+    assert("passed in object is not Ember.Evented", (thing && thing.on && thing.trigger));
+
+    if (typeof(thing) === 'string') {
+      thing = Ember.getOwner(this).lookup(thing);
+    }
+
+    let queueName   = this._queueName(thing, eventName);
+    let queues      = get(this, 'queues');
+    let queue       = getWithDefault(queues, queueName, []);
     queue.push(Ember.assign(info, {callback: callback}));
-    set(this, `queues.${queueName}`, queue);
+
+    if (!get(queues, queueName)) {
+      let _this = this;
+      thing.on(eventName, function(data) { _this._runQueue(queueName, data); });
+    }
+
+    set(queues, queueName, queue);
+    return {queueName};
   },
 
-  runQueue(queueName) {
+  runQueue() {
+
+  },
+
+  _relayEvent(eventName, sound) {
+    this.trigger(eventName, sound);
+  },
+
+  afterRun(thing, eventName, callback) {
+    let queueName  = this._queueName(thing, eventName);
+    this.on(`after:${queueName}`, callback);
+  },
+
+  _queueName(thing, eventName) {
+    let objectName = Ember.String.dasherize(Ember.toString(thing));
+    return `${objectName}:${eventName}`;
+  },
+
+  _queueExists(queueName) {
+    return !!get(this, `queues.${queueName}`);
+  },
+
+  _runQueue(queueName, eventData = {}) {
     let queues = get(this, 'queues');
     let orderedQueue = Ember.A(getWithDefault(queues, queueName, [])).sortBy('priority').slice(); // copy, bro
 
     let runPromise = new RSVP.Promise((resolve) => {
       PromiseRace.start(orderedQueue, function(nextAction, returnSuccess, markFailure) {
-        return RSVP.Promise.resolve(nextAction.callback()).then(result => {
+        return RSVP.Promise.resolve(nextAction.callback(eventData)).then(result => {
           if (result) {
             returnSuccess(result);
           }
@@ -31,6 +68,10 @@ export default Ember.Service.extend({
       })
       .then((results) => resolve(results))
       .catch((results) => resolve(results));
+    });
+
+    runPromise.then((results) => {
+      this.trigger(`after:${queueName}`, results);
     });
 
     return runPromise;
--- tests/unit/services/action-queue-test.js
+++ tests/unit/services/action-queue-test.js
@@ -2,44 +2,36 @@ import { moduleFor, test } from 'ember-qunit';
 import RSVP from 'rsvp';
 import Ember from 'ember';
 
+const TestObject = Ember.Object.extend(Ember.Evented, { });
+
 moduleFor('service:action-queue', 'Unit | Service | action queue', {
   beforeEach() {
     this.subject().set('queues', {});
   }
 });
 
-test('addAction adds action calls to the queue', function(assert) {
+test('addAction requires that object be Evented', function(assert) {
   let service = this.subject();
 
-  let called = [];
-
-  service.addAction('finishedTrack', {priority: 1}, function() {
-    called.push(1);
-  });
-
-  service.addAction('finishedTrack', {priority: 2}, function() {
-    called.push(2);
-  });
-
-  let finishedTrackQueue = service.get('queues.finishedTrack');
-
-  assert.equal(finishedTrackQueue.length, 2, "finishedTrackQueue should have two items in it");
+  assert.throws(function() {
+    service.addAction(Ember.Object.create({}), 'audio-whatever', {}, function() {});
+  }, Error("Assertion Failed: passed in object is not Ember.Evented"));
 });
 
 test('long running actions work as expected', function(assert) {
   let service = this.subject();
   let done    = assert.async();
-
+  let hifiService = new TestObject();
   let called = [];
 
-  service.addAction('finishedTrack', {priority: 1}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 1}, function() {
     return new RSVP.Promise((resolve, reject) => {
       called.push(1);
       Ember.run.later(reject, 500);
     });
   });
 
-  service.addAction('finishedTrack', {priority: 2}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 2}, function() {
     return new RSVP.Promise((resolve) => {
       called.push(2);
       Ember.run.later(() => {
@@ -48,90 +40,105 @@ test('long running actions work as expected', function(assert) {
     });
   });
 
-  service.addAction('finishedTrack', {priority: 2}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 2}, function() {
     return new RSVP.Promise((resolve) => {
       called.push(3);
       resolve(false);
     });
   });
 
-  service.runQueue('finishedTrack').then((results) => {
+  service.afterRun(hifiService, 'audio-finished', function(results) {
     assert.deepEqual(called, [1,2], "first and second actions should have been completed");
     assert.ok(results.failures, "failures array should exist");
     assert.ok(results.success, "success result should exist");
     done();
   });
-});
 
+  hifiService.trigger('audio-finished');
+});
 
 test('runQueue runs the actions in priority order', function(assert) {
   let service = this.subject();
   let done = assert.async();
+  let hifiService = new TestObject();
 
   let called = [];
 
-  service.addAction('finishedTrack', {priority: 1}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 1}, function() {
     called.push(1);
   });
 
-  service.addAction('finishedTrack', {priority: 2}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 2}, function() {
     called.push(2);
   });
 
-  service.addAction('finishedTrack', {priority: 10}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 10}, function() {
     called.push(3);
   });
 
-  service.runQueue('finishedTrack').then(() => {
+  service.afterRun(hifiService, 'audio-finished', function() {
     assert.deepEqual(called, [1,2,3], "actions should have been called in priority order");
     done();
   });
+
+  hifiService.trigger('audio-finished');
 });
 
 test('runQueue only runs actions until one returns true', function(assert) {
   let done = assert.async();
   let service = this.subject();
-
+  let hifiService = new TestObject();
   let called = [];
 
-  service.addAction('finishedTrack', {priority: 1}, function() {
+  service.addAction(hifiService, 'audio-finished',{priority: 1}, function() {
     called.push(1);
     return true;
   });
 
-  service.addAction('finishedTrack', {priority: 2}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 2}, function() {
     called.push(2);
   });
 
-  service.addAction('finishedTrack', {priority: 10}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 10}, function() {
     called.push(3);
   });
 
-  service.runQueue('finishedTrack').then(() => {
+  service.afterRun(hifiService, 'audio-finished', function() {
     assert.deepEqual(called, [1], "only the highest priority action should have been called");
     done();
   });
+
+  hifiService.trigger('audio-finished');
 });
 
 test('queue can be run multiple times', function(assert) {
   let done = assert.async();
   let service = this.subject();
+  let hifiService = TestObject.create();
 
   let called = [];
 
-  service.addAction('finishedTrack', {priority: 1}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 1}, function() {
     called.push(1);
     return true;
   });
 
-  service.addAction('finishedTrack', {priority: 2}, function() {
+  service.addAction(hifiService, 'audio-finished', {priority: 2}, function() {
     called.push(2);
   });
 
-  service.runQueue('finishedTrack').then(() => {
-    service.runQueue('finishedTrack').then(() => {
+  let callCount = 0;
+  var afterRun = function() {
+    if (callCount > 0) {
       assert.deepEqual(called, [1, 1], "priority 1 action should have been called twice");
       done();
-    });
+    }
+    callCount = callCount + 1;
+  };
+
+  service.afterRun(hifiService, 'audio-finished', afterRun);
+  hifiService.trigger('audio-finished');
+  Ember.run.next(() => {
+    hifiService.trigger('audio-finished');
   });
 });
