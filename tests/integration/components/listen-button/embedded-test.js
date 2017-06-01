560b7f1d713dae53bb1869e9f9320c3ea9137027
Jeff Keen <jeff@keen.me>
2017-06-01 18:19:18 -0500

    Test fix

--- tests/integration/components/listen-button/embedded-test.js
+++ tests/integration/components/listen-button/embedded-test.js
@@ -9,7 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{listen-button.embedded}}`);
+  this.render(hbs`{{listen-button/embedded}}`);
 
   assert.equal(this.$().text().trim(), 'Play/Pause');
 });
