d7bf1146c328b73bdab6c488ef098c685602225c
Brian Whitton <me@brianwhitton.com>
2017-07-19 10:26:08 -0400

    import tweak

--- addon/components/stream-banner.js
+++ addon/components/stream-banner.js
@@ -1,10 +1,10 @@
 import Component from 'ember-component';
-import computed from 'ember-computed';
+import { reads } from 'ember-computed';
 import layout from '../templates/components/stream-banner';
 
 export default Component.extend({
   classNames: ['stream-banner'],
   layout,
 
-  activeStream: computed.reads('streams.firstObject')
+  activeStream: reads('streams.firstObject')
 });
