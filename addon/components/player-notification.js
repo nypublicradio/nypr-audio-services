57024597fd5162d4ceca1f967a9f4e7eb28c1243
Brian Whitton <me@brianwhitton.com>
2018-04-26 11:44:59 -0400

    refactor how this event is fired
    use GTM instead of tying into the service and passing the action around

--- addon/components/player-notification.js
+++ addon/components/player-notification.js
@@ -1,14 +1,8 @@
 import Component from '@ember/component';
-import { get } from '@ember/object';
 import layout from '../templates/components/player-notification';
 
 export default Component.extend({
   layout,
   didAnimate: false,
   classNames: ['player-notification', 'js-player-notification'],
-  actions   : {
-    dismiss() {
-      get(this, 'onDismiss')();
-    }
-  }
 });
