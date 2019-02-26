a92c9701cff2fc6069e77186664c9f1e2800d892
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-02-26 12:05:25 -0500

    link "Music Play History" directly to playlist page

--- addon/components/nypr-player-integration.js
+++ addon/components/nypr-player-integration.js
@@ -59,7 +59,7 @@ export default Component.extend({
   streamScheduleUrl    : reads('currentAudio.scheduleUrl'),
   streamPlaylistUrl    : computed('currentAudio.playlistUrl', function() {
     if (get(this,'currentAudio.playlistUrl')) {
-      return `/streams/${get(this, 'currentAudio.id')}`;
+      return `/playlist-daily/?scheduleStation=${get(this, 'currentAudio.id')}`;
     }
   }),
 
